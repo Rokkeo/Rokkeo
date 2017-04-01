c2f7ee104bc86e5c446838bccdbfec09595db23a
Rokke <v.topolovacki@gmail.com>
2017-04-01 04:59:11 +0200

    v4.0

---
+++ projects/CPE/js/show-hide.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.search-btn').click(function(){
+        $('.search-bar').show();
+		
+    $('.hide-btn').click(function(){
+        $('.search-bar').hide();
+    });
+    
+    });
+});
